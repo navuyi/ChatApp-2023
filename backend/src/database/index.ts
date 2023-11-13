ef6235878bf3cfa707fc4b81f98166638f775eb2
navuyi <figlusrafal@gmail.com>
2023-11-13 22:58:43 +0100

    fixed database path to dist

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,19 +1,18 @@
-import * as process from "process"
-import { DataSource } from "typeorm"
-import { User } from "./entities/User"
-
+import * as process from 'process'
+import { DataSource } from 'typeorm'
+import { User } from './entities/User'
 
 export const AppDataSource = new DataSource({
-    type: "sqlite",
-    //host: "localhost",
-    //port: 5432,
-    //username: "test",
-    //password: "test",
-    database: process.env.NODE_ENV==="test" ? "./src/database/database.test.sqlite" : "./src/database/database.sqlite",
-    synchronize: true,  // <-- DO NOT USE IN PRODUCTION
-    logging: true,
-    entities: [User], // <-- all entities have to be imported
-    subscribers: [],
-    migrations: [],
-    dropSchema: true // <-- DO NOT USE IN PRODUCTION
+  type: 'sqlite',
+  //host: "localhost",
+  //port: 5432,
+  //username: "test",
+  //password: "test",
+  database: process.env.NODE_ENV === 'test' ? './dist/database/database.test.sqlite' : './dist/database/database.sqlite',
+  synchronize: true, // <-- DO NOT USE IN PRODUCTION
+  logging: true,
+  entities: [User], // <-- all entities have to be imported
+  subscribers: [],
+  migrations: [],
+  dropSchema: true, // <-- DO NOT USE IN PRODUCTION
 })
